c3dd21d31cd330a8ada9cd388e38db22d5e93a46
Thibaud Huber <thibaud_huber@yahoo.fr>
2020-01-03 12:26:16 +0100

    Switch theme

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentsComponent implements OnInit {
 
+  public theme = 'dune';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeTheme() {
+    this.theme = (this.theme === 'dune') ? 'arrakis' : 'dune';
+    const oldlink = document.getElementsByTagName("link").item(1);
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", `assets/themes/${this.theme}-theme.css`);
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
   }
 
 }
